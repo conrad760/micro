0f69deab6f9bb71c18914e115feed43638b6f702
Conrad Parker <ConradlParker@gmail.com>
2020-04-05 08:46:51 -0500

    added gorila
    Signed-off-by: Conrad Parker <ConradlParker@gmail.com>

--- handlers/product.go
+++ handlers/product.go
@@ -1,12 +1,13 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
 
 	"github.com/conrad760/micro/data"
+	"github.com/gorilla/mux"
 )
 
 // Products introduce a logger for logging
@@ -19,83 +20,39 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	// handle Get
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	// handle Post
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-		return
-	}
-
-	// handle Put
-	if r.Method == http.MethodPut {
-
-		// expect the id in URI
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-
-		}
-
-		p.updateProduct(id, rw, r)
-
-	}
-
-	// catch all
-	rw.WriteHeader(http.StatusNotImplemented)
-
-}
-
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+// GetProducts returns a list of products
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Get all Products")
 
 	listProd := data.GetProducts()
+
 	err := listProd.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Coffee is still brewing...", http.StatusInternalServerError)
 	}
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+// AddProduct adds a product to the products list
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Adding Products to Products List")
 
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(rw, "Coffee is too hot", http.StatusBadRequest)
-	}
-	data.AddProduct(prod)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
 }
 
-func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Updating Products in Products List")
-
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
+// UpdateProduct changes a single product by id
+func (p Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		p.l.Printf("Updating FAILED Json formatting Products List", prod)
-		http.Error(rw, "Coffee is too hot", http.StatusBadRequest)
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
 	}
-	err = data.UpdateProduct(id, prod)
+
+	p.l.Println("Updating Products in Products List", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
 	if err == data.ErrProductNotFound {
 		http.Error(rw, "Product not found", http.StatusNotFound)
 		return
@@ -105,4 +62,26 @@ func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request
 		return
 	}
 
+}
+
+// KeyProduct is a key to the req
+type KeyProduct struct{}
+
+// MiddlewareProductValidation validates Updates on products
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Coffee is too hot", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(rw, r)
+	})
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"github.com/conrad760/micro/handlers"
 )
 
@@ -16,10 +18,21 @@ func main() {
 	// env.Parse()
 
 	l := log.New(os.Stdout, "conrad-api", log.LstdFlags)
+
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
 	srv := &http.Server{
 		Addr:         ":9090",
